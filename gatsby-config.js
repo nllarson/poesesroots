462aedeca8a4ec8a82464b1ac8851231c730b3a2
Nick Larson <nicklas.larson@gmail.com>
2018-08-21 19:24:32 -0500

    add firestore plugin and image plugins

--- gatsby-config.js
+++ gatsby-config.js
@@ -2,5 +2,24 @@ module.exports = {
   siteMetadata: {
     title: 'Gatsby Default Starter',
   },
-  plugins: ['gatsby-plugin-react-helmet'],
+  plugins: [
+    {
+      resolve: 'gatsby-source-firestore',
+      options: {
+        credential: require("./firebase.json"),
+        types: [
+          {
+            type: 'Image',
+            collection: 'images',
+            map: doc => ({
+              name: doc.name,
+              category: doc.category,
+              src: doc.src,
+              project: doc.project              
+            }),
+          }
+        ],
+      },
+    },
+    'gatsby-plugin-react-helmet'],
 }
